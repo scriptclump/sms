7b59f64dcb8f5d43732ecb2bf88e05d5930208b2
Basant Sharma <scriptclump@gmail.com>
2021-06-23 15:54:09 +0530

    Adds the datatable

--- front-end/src/views/content/pages/list.js
+++ front-end/src/views/content/pages/list.js
@@ -1,29 +1,61 @@
 import React from 'react'
 import {
-  CBreadcrumb,
-  CBreadcrumbItem,
+  CBadge,
   CCard,
   CCardBody,
   CCardHeader,
   CLink,
+  CDataTable,
   CCol,
   CRow
 } from '@coreui/react'
 import { DocsLink } from 'src/reusable'
 import routes from '../../../routes'
+import usersData from '../../users/UsersData';
+
+const getBadge = status => {
+  switch (status) {
+    case 'Active': return 'success'
+    case 'Inactive': return 'secondary'
+    case 'Pending': return 'warning'
+    case 'Banned': return 'danger'
+    default: return 'primary'
+  }
+}
+const fields = ['name','registered', 'role', 'status']
 
 const PageList = () => {
   return (
     <CRow>
       <CCol xs="12">
         <CCard>
+          {/* <button class="btn btn-primary">Add New Page</button> */}
           <CCardHeader>
             Pages
           </CCardHeader>
-          <CCardBody>           
-                        
+          <CCardBody>
+            <CDataTable
+              items={usersData}
+              fields={fields}
+              dark
+              hover
+              striped
+              bordered
+              itemsPerPage={10}
+              pagination
+              scopedSlots = {{
+                'status':
+                  (item)=>(
+                    <td>
+                      <CBadge color={getBadge(item.status)}>
+                        {item.status}
+                      </CBadge>
+                    </td>
+                  )
+              }}
+            />
           </CCardBody>
-        </CCard>
+          </CCard>
       </CCol>
     </CRow>
   )
